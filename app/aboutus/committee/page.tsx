3b0eaa8311c269bc55ab2943df83a648db0e513a
vasusadariya <sadariyavasu5@gmail.com>
2026-01-01 14:26:35 +0530

    add export function in empty files and solve build errors

---
+++ app/aboutus/committee/page.tsx
@@ -0,0 +1,7 @@
+export default function CommitteePage() {
+  return (
+    <div>
+      <h1>Committee</h1>
+    </div>
+  );
+}
---
+++ app/aboutus/theme/page.tsx
@@ -0,0 +1,7 @@
+export default function ThemePage() {
+  return (
+    <div>
+      <h1>Theme</h1>
+    </div>
+  );
+}
